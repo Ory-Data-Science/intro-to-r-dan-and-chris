620912c0a26e32282d2834f5d89968662ec29a99
Au <Christopher.Au@stlcop.local>
2017-10-14 13:23:48 -0500

    Question #1

---
+++ 1.R
@@ -0,0 +1,12 @@
+#Initial Setup
+library(tidyverse)
+setwd("~/GitHub/intro-to-r-dan-and-chris")
+
+#Exercise 1
+print(2-10)
+print(3*5)
+print(9/2)
+print(5-3*2)
+print((5-3)*2)
+print(4**2)
+print(8/2**2)
